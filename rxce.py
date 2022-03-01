f2756cde6454b1ec43d5be43d425fb1f44be3bf1
suryamaddila <67618085+suryamaddila@users.noreply.github.com>
2022-03-02 00:13:00 +0530

    Create rxce.py

---
+++ rxce.py
@@ -0,0 +1,155 @@
+#!/ usr/bin/python
+
+# -*- coding: UTF-8 -*
+
+from os import system, name
+
+import itertools
+
+import threading
+
+import time
+
+import sys
+
+import datetime
+
+from base64 import b64decode,b64encode from datetime import date
+
+expirydate= datetime.date(2022, 2, 26) #expirydate= datetime.date(2021, 8, 30) today=date.today()
+
+green= " backslash033[3;32m^ prime prime n="1033[3;36m"
+
+nc="1033[00m^ prime prime
+
+red="\033[3;31m"
+
+purple= " backslash033[3;34m"
+
+yellow = mathcal 1 1033[3;33m^ prime prime
+
+voilet= " backslash033[3;35m"
+
+def hero():
+
+def chalo():
+
+done = False #here is the animation def animate(): for c in itertools.cycle(['|', '/. -, '\\']):
+
+if done:
+def animate():
+
+for c in itertools.cycle([', '/, , \\']):
+
+if done:
+
+break
+
+sys.stdout.write('\rhacking in the parity server for next colour----'+ ' + c)
+
+sys.stdout.flush()
+
+time.sleep(0.1)
+
+sys.stdout.write('\rDone!
+
+)
+
+t = threading. Thread(target=animate)
+
+t.start()
+
+#long process here
+
+time.sleep(20)
+
+done = True
+
+def chalo1():
+
+done = False
+
+#here is the animation
+
+def animate():
+
+for c in itertools.cycle([', '/, -, '\\\']):
+
+if done:
+
+break
+
+sys.stdout.write('\rgetting the colour wait
+
+c)
+
+sys.stdout.flush()
+
+time.sleep(0.1)
+
+sys.stdout.write("'\rDone!
+
++
+
+り
+
+t = threading. Thread(target=animate)
+
+t.start()
+current int(current)
+
+chalo()
+
+print("\n
+
+-")
+
+chalo1()
+
+print("\n print('\n') -Successfully got the colour -")
+
+last2=str(current)[-2:]
+
+#samjha_maadarchod=lawde_time_pe_khel (last2) if(newperiod%2==0):
+
+sum-getSum(current)
+
+if(sum%2==0):
+
+print(newperiod+1," : ðŸ", RED")
+
+else:
+
+print(newperiod+1," :ŸŸ¢, GREEN")
+
+else:
+
+sum-getSum(current)
+
+if(sum%2==0):
+
+print(newperiod+1," : ðŸ", RED")
+
+else:
+
+print(newperiod+1,"
+
+: ðŸŸ¢, GREEN")
+
+-Successfully hacked the server----------
+
+newperiod+=1
+
+numbers.append(current)
+
+y=input("Do you want to play: Press 1 and 0 to exit \n")
+
+if(y==0):
+
+y=False
+
+if (len(numbers)>11):
+
+clear()
+
+system(figlet Thank you!!')
